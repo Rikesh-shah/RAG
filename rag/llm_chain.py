e14a1aa36d52d000a036fb45d9e7de3085ca2980
Rikesh-shah <rikeshshah2075@gmail.com>
2025-07-05 07:35:59 +0545

    prompt template

--- rag/llm_chain.py
+++ rag/llm_chain.py
@@ -1,3 +1,4 @@
+from langchain.prompts import PromptTemplate
 from langchain.chains import ConversationalRetrievalChain
 from langchain_google_genai import ChatGoogleGenerativeAI
 from rag import load_vectorstore
@@ -12,6 +13,25 @@ def load_llm():
 def build_chain():
     vectorstore = load_vectorstore()
     retriever = vectorstore.as_retriever(search_kwargs={"k": 4})
+    prompt_template = PromptTemplate.from_template("""
+                        You are a helpful and factual AI assistant specializing in medical information.
+
+                        Use only the provided context to answer the user’s question. Do NOT use any prior knowledge. If the context does not contain the answer, say you don't know.
+
+                        For every paragraph, add a citation to the source like this: [filename.pdf, Page X].
+
+                        At the end of your response, include a list of all sources used in the format:
+                        - filename.pdf (Page X)
+
+                        ---
+
+                        Context:
+                        {context}
+
+                        Question: {question}
+
+                        Answer:
+                        """)
     llm = load_llm()
     return ConversationalRetrievalChain.from_llm(
         llm=llm,
